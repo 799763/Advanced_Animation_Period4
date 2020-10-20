b7efa4680df0fc9e17b40ef4450e2746cd9d8299
Fernando <54371477+799763@users.noreply.github.com>
2020-10-19 20:05:23 -0700

    19 Oct 2020 - 8:05pm

--- 3.7 Oscillation/oscillator.js
+++ 3.7 Oscillation/oscillator.js
@@ -1,23 +1,23 @@
 //  Bubble constructor function +++++++++++++++++++++++++++++
 function Bubble(){// change parameters
     this.angle = new JSVector();
-    this.velocity = new JSVector(random(-0.05, 0.05), random(-0.05, 0.05));
-    this.amplitude = new JSVector(random(canvas.width/2), random(canvas.height/2));
-    this.angle.add(velocity);
+    this.velocity = new JSVector(Math.random(-0.05, 0.05), Math.random(-0.05, 0.05));
+    this.amplitude = new JSVector(Math.random(canvas.width/2), Math.random(canvas.height/2));
+    this.angle.add(this.velocity);
+    let ctx = Game.ctx;
 
   //  placing methods in the prototype (every bubble shares functions)
 Bubble.prototype.run = function(){
-    this.checkEdges();
     this.render();
   }
 
 // draw the bubble on the canvas
 Bubble.prototype.render = function(){
-    let x = Math.sin(angle.x)*amplitude.x;
-    let x = Math.sin(angle.x)*amplitude.y;
+     x = Math.sin(this.angle.x)*this.amplitude.x;
+     x = Math.sin(this.angle.x)*this.amplitude.y;
 
-    pushMatrix();
-    translate(canvas.width/2, canvas.height/2);
+    //pushMatrix();
+    ctx.translate(canvas.width/2, canvas.height/2);
     ctx.strokeStyle = 'rgba(255,255,0)';
     ctx.fillStyle = 'rgba(255, 255, 0)';
     ctx.beginPath();
@@ -25,6 +25,6 @@ Bubble.prototype.render = function(){
     stroke(0);
     fill(175);
     line(0,0,x,y)
-    popMatrix();
+    //popMatrix();
   }
 }
