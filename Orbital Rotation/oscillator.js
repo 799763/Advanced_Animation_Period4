80ca0bd669c030908d2a3b44eb4e0de4091c7e2c
Fernando <54371477+799763@users.noreply.github.com>
2020-10-22 18:49:57 -0700

    22 Oct. 6:49pm

--- Orbital Rotation/oscillator.js
+++ Orbital Rotation/oscillator.js
@@ -1,34 +1,42 @@
 //  Oscillator constructor function +++++++++++++++++++++++++++++
-function Oscillator(g){// change parameters
-    this.angle = new JSVector();
-    this.velocity = new JSVector(Math.random()*0.06 - 0.03, Math.random()*0.06 - 0.03);
-    this.amplitude = new JSVector(Math.random()*g.canvas.width/2, Math.random()*g.canvas.height/2);
-}
+function Oscillator(x, y, dx, dy, r, c, n){// change parameters
+  this.loc = new JSVector(x, y);
+  this.vel = new JSVector(dx, dy);
+  this.acc = new JSVector();
+  this.rad = r;
+  this.orbitAngle = Math.random() * Math.PI;
+  this.clr = c;
+  this.orbiters = [];
+
+  for(let i = 0; i < n; i++){
+      let a = i * (Math.PI * 2) / numOrbs + this.orbiters;
+      let angleVel = numOrbs * 0.01;
+      this.orbitors.push(new Orbiter(this, 4, 25, a, a));
+  }
+
+}//Oscillator end++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
   //  placing methods in the prototype (every Oscillator shares functions)
 Oscillator.prototype.run = function(){
     this.update();
     this.render();
+    this.checkEdges();
   }
 
-// draw the Oscillator on the canvas
-Oscillator.prototype.render = function(){
-     let x = Math.cos(this.angle.x)*this.amplitude.x;
-     let y = Math.sin(this.angle.y)*this.amplitude.y;
+Oscillator.prototype.checkEdges = function(){
 
-    //pushMatrix();
-    let ctx = game.ctx;
-    ctx.save();
-    ctx.translate(game.canvas.width/2, game.canvas.height/2);
-    ctx.strokeStyle = 'rgba(255,255,0)';
-    ctx.fillStyle = 'rgba(255, 255, 0)';
-    ctx.beginPath();
-    ctx.arc(x, y, 20, Math.PI*2, 0, false);
-    ctx.stroke();
-    ctx.fill();
-    ctx.restore();
-    //line(0,0,x,y)
-    //popMatrix();
   }
-Oscillator.prototype.update = function(){
-  this.angle.add(this.velocity);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function Orbiter(){
+
 }
+
+  // draw the Oscillator on the canvas
+  Oscillator.prototype.render = function(){//Talk in class
+
+    }
+    Oscillator.prototype.update = function(){ //Talk in class
+
+    }
