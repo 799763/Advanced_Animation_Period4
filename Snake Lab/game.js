89ca1ea0efb63e9e126b739c7475d28dc76995e1
Fernando <54371477+799763@users.noreply.github.com>
2020-10-30 10:32:05 -0700

    Add files via upload

---
+++ Snake Lab/game.js
@@ -0,0 +1,46 @@
+function Game(){
+
+    this.gamePaused = false;    // the game may be paused or not
+    this.ga = new GameArea();   // create all the dom elements
+    // get the canvas as a property of the game
+    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/canvas
+    this.canvas = document.getElementById('canvas');
+    // get the context
+    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+    this.ctx = this.canvas.getContext('2d'); // This is the context
+
+    //   create the array of bubble objects
+    this.oscillators = [];
+    let numBubbles = 15;
+    for(var i = 0; i < numBubbles; i++){
+        this.oscillators.push(new Oscillator(this)); // add new bubble to array
+    }
+
+    //  Add event handlers to all tile objects
+    for(let i = 0; i < this.ga.tiles.length; i++){
+        this.ga.tiles[i].addEventListener('mouseover', // mouseover is the name of an event
+                                        function(){//  JavaScript has anonymous functions
+                                          //  'this' is the listener target object: tile
+                                          //  'this' does not refer to the game object
+                                          this.style.backgroundColor = "#ac8fe3"
+                                        },
+                                        false);
+        this.ga.tiles[i].addEventListener('mouseout', function(){
+            this.style.backgroundColor = "#d5dee0"
+          },false);
+        this.ga.tiles[i].addEventListener('click', function(){
+            game.gamePaused = !game.gamePaused;
+            console.log("Mouse Clicked");
+          },false);
+    }
+
+}//++++++++++++++++++++++  end Bubbles constructor
+
+// function to run the game each animation cycle
+Game.prototype.run = function(){
+  if(!this.gamePaused){
+    for(let i = 0; i < this.oscillators.length; i++){
+      this.oscillators[i].run(); //run each bubble
+   }
+  }
+}
