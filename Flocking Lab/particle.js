ee99ee72b6a8463ba9ef7a18161315f43984930c
Fernando <54371477+799763@users.noreply.github.com>
2020-11-17 10:39:56 -0800

    Add files via upload

---
+++ Flocking Lab/particle.js
@@ -0,0 +1,33 @@
+//All parameters are currently vectors
+function Particle(location){
+   this.loc = location.copy();
+   this.vel = new JSVector(Math.random()*2-1, -2);
+   this.acc = new JSVector(0, 0.05);
+   this.rad = 10; // size of particle
+   this.red = 240;
+   this.green = 240;
+   this.blue = 0;
+   this.lifeSpan = Math.floor(Math.random()*255);
+   this.isDead = false;
+}
+   Particle.prototype.run = function(){
+     this.update();
+     this.render();
+   }
+
+   Particle.prototype.update = function(){
+     this.vel.add(this.acc);
+     this.loc.add(this.vel);
+     this.lifeSpan -= 2;
+     if(this.lifeSpan < 0){
+        this.isDead = true;
+     }
+   }
+
+   Particle.prototype.render = function(){
+     let ctx = game.ctx;
+     ctx.fillStyle = "rgba(" + this.red + "," + this.green + "," + this.blue + "," + this.lifeSpan/255 + ")";
+     ctx.beginPath();
+     ctx.arc(this.loc.x, this.loc.y, this.rad, 0, Math.PI*2);
+     ctx.fill(); //removed "this.lifeSpan" from inside parenthesees
+   }
