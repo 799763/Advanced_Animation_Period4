3862c52c67f8740bc11c3899f2671d9b1454c22a
Fernando <54371477+799763@users.noreply.github.com>
2020-11-17 10:37:10 -0800

    Nov 17, 10:36am Particle Lab
    Changed final update, code now works

--- Particle Lab/particleSystem.js
+++ Particle Lab/particleSystem.js
@@ -10,7 +10,7 @@ ParticleSystem.prototype.run = function(){
   for(var i = this.particles.length-1; i > 0; i--){
     this.particles[i].run();
     if(this.particles[i].isDead){
-        this.particles[i].splice(i, 1);
+        this.particles.splice(i, 1);
         this.particles.push(new Particle(this.emitter));
     }
   }
