052d614153e8284dbf4290ce7d36898d27512548
Fernando <54371477+799763@users.noreply.github.com>
2020-11-19 21:05:47 -0800

    Nov 19. 9:04pm, Calling it a day
    It's getting late and I still have more assingments left. This took longer than expected. Going to sleep If im able to complete the assignemts I have left. Will ask Mr. Ettlin/ Mr. Kary for help in class tomorrow.

---
+++ Flocking Lab 2.0/Vehicle.js
@@ -0,0 +1,54 @@
+function Vehichle(){
+  this.location;
+  this.velocity;
+  this.acceleration;
+  this.maxSpeed;
+  this.maxForce;
+  this.desired = new JSVector(0,0);
+  this.steer = new JSVector(0,0);
+
+}
+
+Vehicle.prototype.run = function(){
+
+}
+
+Vehicle.prototype.update = function(){
+  this.velocity.add(this.acceleration);
+  this.velocity.limit(this.maxSpeed);
+  this.location.add(this.velocity);
+  this.acceleration.mult(0);
+}
+
+Vehicle.prototype.render = function(){
+  let ctx = game.ctx;
+  ctx.fillStyle = "red";
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = 1;
+  ctx.beginPath();
+  ctx.arc(this.loc.x, this.loc.y, 5, Math.PI * 2, 0, false);
+  ctx.stroke();
+  ctx.fill();
+}
+
+Vehicle.prototype.seek = function(target){
+  this.desired = JSVector.sub(target, this.location);
+  this.desired = JSVector.divide(2);
+  let d = this.desired.getMagnitude();
+  this.desired.normalize();
+  if(d < 100){
+    let m = this.desired.distance(target);
+    this.desired.multiply(m);
+  }else{
+    this.desired.multiply(this.maxSpeed);
+  }
+
+  this.steer = JSVector.sub(0.5, this.velocity);
+  this.steer.limit(this.maxForce);
+
+  this.applyForce(this.steer);
+}
+
+Vehicle.prototype.applyForce = function(force){
+  this.acceleration.add(force);
+}
---
+++ Flocking Lab 2.0/flowField.js
@@ -0,0 +1,6 @@
+function FlowField(){
+  this.field = new JSVector[][]();
+  let cols = canvas.width/this.resolution;
+  let rows = canvas.width/this.resolution;
+  let resolution; = 10
+}
