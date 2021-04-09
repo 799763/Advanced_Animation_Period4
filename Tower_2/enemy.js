47c01b0dd85a49200644229304ece7576e82aa9f
Fernando <54371477+799763@users.noreply.github.com>
2021-04-08 18:19:51 -0700

    Update enemy.js

--- Tower_2/enemy.js
+++ Tower_2/enemy.js
@@ -8,8 +8,8 @@ function Enemy(canvas,rad, clr){
     let y = (Math.random() * (canvas.height - 2*this.radius)) + this.radius;
     this.loc = new JSVector(x,y);
     // random velocity
-    let dx = (Math.random() * 6) -2;
-    let dy = (Math.random() * 6) -2;
+    let dx = (Math.random() * 3) -2;
+    let dy = (Math.random() * 3) -2;
     this.vel = new JSVector(dx,dy);
 }
 
